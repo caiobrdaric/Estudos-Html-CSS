94e2b7a9e2100e3326fc0671e7322ef89a78f991
Caio Brdaric <caiobrdaric@gmail.com>
2022-01-21 20:03:00 -0300

    primeiro commit

---
+++ post.js
@@ -0,0 +1,21 @@
+function fazPost (url, body){
+
+}
+
+function cadastraUsuario() {
+    preventDefaut()
+     
+    let nome = document.getElementById("nome")
+    let email = document.getElementById("email")
+    let telefone = document.getElementById("telefone")
+    let cidade = document.getElementById("cidade")
+    let estado = document.getElementById("estado")
+    let endereço = document.getElementById("endereço")
+    console.log(nome)
+    console.log(email)
+    console.log(telefone)
+    console.log(cidade)
+    console.log(estado)
+    console.log(endereço)
+    
+}
